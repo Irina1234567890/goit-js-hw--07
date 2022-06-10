9dbc9be551f9e3c01a934fe4e87463e9eeb3bc79
Irina1234567890 <irina3213@gmail.com>
2022-06-10 19:17:17 +0300

    GALLERY

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,5 +3,32 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
 
-const gallerryItem = document.querySelector('.gallery');
 
+const gallery = document.querySelector('.gallery');
+
+
+function onCreateItem(galleryItems) {
+  return galleryItems.map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+    <a class="gallery__link" href='${original}'>
+      <img
+        class="gallery__image"
+        src='${preview}'
+        data-source='${original}'
+        alt='${description}'
+        />
+        </a>
+    </div>`}).join('');
+}
+
+const item = onCreateItem(galleryItems);
+gallery.insertAdjacentHTML('beforeend', item);
+gallery.addEventListener('click', onImageClick);
+
+function onImageClick(evt) {
+  evt.preventDefault();
+  const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">`)
+
+    instance.show();
+}
