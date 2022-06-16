cf339c33628d5324abe3229a5999eecf63cd1791
Irina1234567890 <irina3213@gmail.com>
2022-06-16 11:07:50 +0300

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -25,26 +25,46 @@ gallery.addEventListener('click', onImageClick);
 
 function onImageClick(evt) {
   evt.preventDefault();
+
+  if (evt.target.nodeName !== 'IMG') { return; };
+
   const instance = basicLightbox.create(`
     <img src="${evt.target.dataset.source}" width="800" height="600">`)
 
     instance.show();
+  
 }
 
-// function onCloseModal() {
-//   window.removeEventListener("keydown", onEscKeyModal);
-//   document.body.classList.close(".basicLightbox--visible");
-// }
+ 
+function onCloseModal() {
+  window.removeEventListener("keydown", onEscKeyModal);
+  document.body.classList.close(".basicLightbox--visible");
+}
 
-// function onEscKeyModal(event) {
-//   if (event.code === "Escape") {
-//     onCloseModal();
-//   }
-// }
+function onEscKeyModal(event) {
+  if (event.code === "Escape") {
+    onCloseModal();
+  }
+  
+}
 
 const instance = basicLightbox.create(html, {
   onShow: (instance) => console.log('onShow', instance),
   onClose: (instance) => console.log('onClose', instance)
 });
 
+
+// function isEscapeDown (evt) {
+//   if (evt.code === 'Escape') {
+//     instance.close(window.removeEventListener('keydown', isEscapeDown));
+//   };
+// }
+
 instance.show((instance) => console.log('finished show()', instance));
+
+
+
+// instance.close(window.removeEventListener('keydown', isEscapeDown));
+// function noModal(evt) {
+//   if (evt.target.nodeName !== ‘IMG’) { return; };
+// }
